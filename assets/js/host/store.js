0bebe500570c0186d9c14b787afb8ae21cbe8e04
adz5555 <adam.mains@sky.com>
2022-04-23 01:01:10 +0100

    Update store.js

--- assets/js/host/store.js
+++ assets/js/host/store.js
@@ -1,14 +1,21 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-import reducer from './reducer'
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import reducer from "./reducer";
 
-const loggerMiddleware = createLogger()
+const loggerMiddleware = createLogger();
+
+let middleware = [];
+if (process.env.NODE_ENV === 'development') {
+  middleware = [...middleware, thunkMiddleware, loggerMiddleware];
+} else {
+  middleware = [...middleware, thunkMiddleware];
+}
 
 export default function configureStore(preloadedState) {
   return createStore(
     reducer,
     preloadedState,
-    applyMiddleware(thunkMiddleware, loggerMiddleware)
-  )
+    applyMiddleware(...middleware)
+  );
 }
